0c54da99be38c7a372ef387faccae3cf06f653ee
Alberto Becerra <alberto88vala@gmail.com>
2026-06-16 18:04:57 -0600

    4 noruega

--- src/pages/QuinielaManual.jsx
+++ src/pages/QuinielaManual.jsx
@@ -6,7 +6,7 @@ export default function QuinielaManual() {
 
   const marcadorMundial = {
     partido: " Irak VS Noruega",
-    resultado: " 1 - 3"
+    resultado: " 1 - 4"
   };
 
   const partidos = [
@@ -158,7 +158,7 @@ export default function QuinielaManual() {
     15: "1 - 1",
     16: "2 - 2",
     17: "3 - 1",
-    18: "1 - 3",
+    18: "1 - 4",
     
   };
 
