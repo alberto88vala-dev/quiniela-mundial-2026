81e635e9345a3f09ae8f61f22e15f8e4d0863070
alberto88vala-dev <alberto08vala@outlook.com>
2026-07-02 17:22:09 -0600

    españa

--- src/pages/QuinielaManual.jsx
+++ src/pages/QuinielaManual.jsx
@@ -16,6 +16,7 @@ export default function QuinielaManual() {
     80: { regular: "2 - 1" }, // Inglaterra vs RD Congo
     82: { regular: "2 - 2", penales: "3 - 2" }, // Bélgica vs Senegal
     81: { regular: "2 - 0" }, // USA vs Bosnia
+    84: { regular: "3 - 0" }, // España vs Austria
     };
 
   const partidos = [
