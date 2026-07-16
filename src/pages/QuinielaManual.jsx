1a42b568d842ce8dc0a6238590f106cd7514e4d8
alberto88vala-dev <alberto08vala@outlook.com>
2026-07-15 21:17:17 -0600

    argentina

--- src/pages/QuinielaManual.jsx
+++ src/pages/QuinielaManual.jsx
@@ -35,7 +35,8 @@ export default function QuinielaManual() {
     99: { regular: "1 - 1", penales: "1 - 2" }, // G-91 vs G-92
     100: { regular: "1 - 1", penales: "3 - 1" }, // G-95 vs G-96
     101: { regular: "0 - 2" }, // G-97 vs G-98
-    
+    102: { regular: "1 - 2" }, // G-99 vs G-100
+
 
     };
 
